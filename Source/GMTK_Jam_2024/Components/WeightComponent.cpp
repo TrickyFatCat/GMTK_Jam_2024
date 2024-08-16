784eda48ee6e0a8478d96351c1cb4e78b6a15803
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-08-16 23:58:10 +0300

    Create weight component

---
+++ Source/GMTK_Jam_2024/Components/WeightComponent.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WeightComponent.h"
+
+
+UWeightComponent::UWeightComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UWeightComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+void UWeightComponent::AddWeight(const int32 DeltaWeight)
+{
+	if (DeltaWeight <= 0)
+	{
+		return;
+	}
+
+	CurrentWeight += DeltaWeight;
+	OnWeightAdded.Broadcast(this, CurrentWeight, DeltaWeight);
+}
+
+void UWeightComponent::RemoveWeight(const int32 DeltaWeight)
+{
+	if (DeltaWeight <= 0)
+	{
+		return;
+	}
+
+	CurrentWeight -= DeltaWeight;
+	CurrentWeight = FMath::Max(CurrentWeight, 0);
+	OnWeightRemoved.Broadcast(this, CurrentWeight, DeltaWeight);
+}
---
+++ Source/GMTK_Jam_2024/Components/WeightComponent.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WeightComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnWeightAddedDynamicSignature,
+                                               UWeightComponent*, Component,
+                                               const int32, NewWeight,
+                                               const int32, DeltaWeight);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnWeightRemovedDynamicSignature,
+                                               UWeightComponent*, Component,
+                                               const int32, NewWeight,
+                                               const int32, DeltaWeight);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GMTK_JAM_2024_API UWeightComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UWeightComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnWeightAddedDynamicSignature OnWeightAdded;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnWeightRemovedDynamicSignature OnWeightRemoved;
+	
+	UFUNCTION(BlueprintCallable)
+	void AddWeight(const int32 DeltaWeight);
+
+	UFUNCTION(BlueprintCallable)
+	void RemoveWeight(const int32 DeltaWeight);
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta=(AllowPrivateAccess, ClampMin="0"))
+	int32 CurrentWeight = 0;
+};
