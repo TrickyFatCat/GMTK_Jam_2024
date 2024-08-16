0c45cdfefe0e9dea79d28b3bfc4d2bce297f6c61
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-08-16 23:12:06 +0300

    Create base entity class

---
+++ Source/GMTK_Jam_2024/Actors/EntityBase.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EntityBase.h"
+
+#include "Components/BoxComponent.h"
+
+
+AEntityBase::AEntityBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Collision = CreateDefaultSubobject<UBoxComponent>(FName("Collision"));
+	SetRootComponent(Collision);
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(FName("StaticMesh"));
+	StaticMeshComponent->SetupAttachment(GetRootComponent());
+}
---
+++ Source/GMTK_Jam_2024/Actors/EntityBase.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EntityBase.generated.h"
+
+class UBoxComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class GMTK_JAM_2024_API AEntityBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AEntityBase();
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UBoxComponent> Collision = nullptr;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent = nullptr;
+};
