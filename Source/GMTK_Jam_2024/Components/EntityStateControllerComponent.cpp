06b3c25d1a277410f2ca9539bc448e579eeb7419
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-08-17 18:14:51 +0300

    Create state machine for entity

---
+++ Source/GMTK_Jam_2024/Components/EntityStateControllerComponent.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EntityStateControllerComponent.h"
+
+
+UEntityStateControllerComponent::UEntityStateControllerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	bWantsInitializeComponent = true;
+}
+
+void UEntityStateControllerComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+void UEntityStateControllerComponent::ActivateEntity()
+{
+	ChangeState(EEntityState::Active);
+}
+
+void UEntityStateControllerComponent::DisableEntity()
+{
+	ChangeState(EEntityState::Disabled);
+}
+
+void UEntityStateControllerComponent::ChangeState(const EEntityState NewState)
+{
+	if (CurrentState == NewState)
+	{
+		return;
+	}
+
+	CurrentState = NewState;
+	OnStateChanged.Broadcast(this, NewState);
+}
---
+++ Source/GMTK_Jam_2024/Components/EntityStateControllerComponent.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EntityStateControllerComponent.generated.h"
+
+UENUM()
+enum class EEntityState : uint8
+{
+	Inactive,
+	Active,
+	Disabled
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnStateChangedDynamicSignature,
+                                             UEntityStateControllerComponent*, Component,
+                                             EEntityState, NewState);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GMTK_JAM_2024_API UEntityStateControllerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEntityStateControllerComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnStateChangedDynamicSignature OnStateChanged;
+
+	UFUNCTION(BlueprintGetter)
+	EEntityState GetEntityState() const { return CurrentState; }
+
+	UFUNCTION(BlueprintCallable)
+	void ActivateEntity();
+
+	UFUNCTION(BlueprintCallable)
+	void DisableEntity();
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetCurrentState)
+	EEntityState CurrentState = EEntityState::Inactive;
+
+	UFUNCTION()
+	void ChangeState(const EEntityState NewState);
+};
