725b939e6ca50a9de6ddfe56f25b4a3ac40c96cb
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-08-17 14:34:25 +0300

    Create FailuresCounterComponent

---
+++ Source/GMTK_Jam_2024/Components/FailuresCounterComponent.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FailuresCounterComponent.h"
+
+
+UFailuresCounterComponent::UFailuresCounterComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UFailuresCounterComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+void UFailuresCounterComponent::IncreaseFailureCount()
+{
+	FailuresNumber += 1;
+	OnFailuresNumberIncreased.Broadcast(this, FailuresNumber);
+
+	if (FailuresNumber > FailuresThreshold)
+	{
+		OnFailuresThresholdReached.Broadcast(this);
+	}
+}
---
+++ Source/GMTK_Jam_2024/Components/FailuresCounterComponent.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "FailuresCounterComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnFailuresNumberIncreasedDynamicSignature,
+                                             UFailuresCounterComponent*, Componet,
+                                             const int32, NewFailureNumber);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnFailuresThresholdReachedDynamicSiganture,
+                                            UFailuresCounterComponent*, Component);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GMTK_JAM_2024_API UFailuresCounterComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFailuresCounterComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnFailuresNumberIncreasedDynamicSignature OnFailuresNumberIncreased;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnFailuresThresholdReachedDynamicSiganture OnFailuresThresholdReached;
+
+	UFUNCTION(BlueprintCallable)
+	void IncreaseFailureCount();
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetFailuresNumber() const { return FailuresNumber; }
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetFailuresThreshold() const { return FailuresThreshold; }
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetFailuresNumber)
+	int32 FailuresNumber = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter=GetFailuresThreshold)
+	int32 FailuresThreshold = 5;
+};
