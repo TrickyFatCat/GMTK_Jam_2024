d2eb683f06e2dd042cf66a8e2955bb8402e0d974
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-08-17 07:35:38 +0300

    Create EntityManagerComponent

---
+++ Source/GMTK_Jam_2024/Components/EntityManagerComponent.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EntityManagerComponent.h"
+
+#include "GMTK_Jam_2024/Actors/EntityBase.h"
+
+
+UEntityManagerComponent::UEntityManagerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UEntityManagerComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+bool UEntityManagerComponent::AddEntity(AEntityBase* Entity)
+{
+	if (!IsValid(Entity) || AddedEntities.Contains(Entity))
+	{
+		return false;
+	}
+
+	AddedEntities.AddUnique(Entity);
+	return true;
+}
+
+bool UEntityManagerComponent::RemoveEntity(AEntityBase* Entity)
+{
+	if (!IsValid(Entity) || !AddedEntities.Contains(Entity))
+	{
+		return false;
+	}
+
+	AddedEntities.RemoveSingle(Entity);
+	return true;
+}
---
+++ Source/GMTK_Jam_2024/Components/EntityManagerComponent.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EntityManagerComponent.generated.h"
+
+
+class AEntityBase;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnEntityAddedDynamicSignature,
+                                             UEntityManagerComponent*, Component,
+                                             AEntityBase*, Entity);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnEntityRemovedDynamicSignature,
+                                             UEntityManagerComponent*, Component,
+                                             AEntityBase*, Entity);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GMTK_JAM_2024_API UEntityManagerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEntityManagerComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnEntityAddedDynamicSignature OnEntityAdded;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnEntityRemovedDynamicSignature OnEntityRemoved;
+	
+	UFUNCTION(BlueprintCallable)
+	bool AddEntity(AEntityBase* Entity);
+
+	UFUNCTION(BlueprintCallable)
+	bool RemoveEntity(AEntityBase* Entity);
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly)
+	TArray<AEntityBase*> AddedEntities;
+};
