913b0850c9303225bf615fa20b8cb556a02c0c10
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-08-17 08:01:14 +0300

    Create ScaleBalance actor

---
+++ Source/GMTK_Jam_2024/Actors/ScaleBalance.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScaleBalance.h"
+
+#include "EntityBase.h"
+#include "ScaleBowl.h"
+
+
+AScaleBalance::AScaleBalance()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AScaleBalance::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+bool AScaleBalance::AddEntity(AEntityBase* Entity) const
+{
+	if (!IsValid(Entity) || !IsValid(RightScaleBowl))
+	{
+		return false;
+	}
+
+	return RightScaleBowl->AddEntity(Entity);
+}
+
+bool AScaleBalance::RemoveEntity(AEntityBase* Entity) const
+{
+	if (!IsValid(Entity) || !IsValid(RightScaleBowl))
+	{
+		return false;
+	}
+
+	return RightScaleBowl->RemoveEntity(Entity);
+}
---
+++ Source/GMTK_Jam_2024/Actors/ScaleBalance.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ScaleBalance.generated.h"
+
+class AEntityBase;
+class AScaleBowl;
+
+UCLASS()
+class GMTK_JAM_2024_API AScaleBalance : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AScaleBalance();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	bool AddEntity(AEntityBase* Entity) const;
+	
+	UFUNCTION(BlueprintCallable)
+	bool RemoveEntity(AEntityBase* Entity) const;
+	
+protected:
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	AScaleBowl* LeftScaleBowl = nullptr;
+	
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly)
+	AScaleBowl* RightScaleBowl = nullptr;
+};
