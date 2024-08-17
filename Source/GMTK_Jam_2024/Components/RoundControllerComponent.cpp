3d5fdfe8e23d86aa31afa619cf0d7011fd7c1758
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-08-17 13:10:09 +0300

    Create RoundControllerComponent

---
+++ Source/GMTK_Jam_2024/Components/RoundControllerComponent.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RoundControllerComponent.h"
+
+
+URoundControllerComponent::URoundControllerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void URoundControllerComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+void URoundControllerComponent::StartRound()
+{
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+
+	if (TimerManager.IsTimerActive(RoundTimer))
+	{
+		return;
+	}
+
+	RoundIndex += 1;
+	TimerManager.SetTimer(RoundTimer, this, &URoundControllerComponent::FinishRound, RoundDuration, false);
+	OnRoundStarted.Broadcast(this, RoundIndex);
+}
+
+void URoundControllerComponent::FinishRound()
+{
+	OnRoundFinished.Broadcast(this, RoundIndex);
+}
---
+++ Source/GMTK_Jam_2024/Components/RoundControllerComponent.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "RoundControllerComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnRoundStartedDynamicSignature,
+                                             URoundControllerComponent*, Component,
+                                             const int32, RoundIdx);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnRoundFinishedDynamicSignature,
+                                             URoundControllerComponent*, Component,
+                                             const int32, RoundIdx);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GMTK_JAM_2024_API URoundControllerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	URoundControllerComponent();
+
+protected:
+	virtual void InitializeComponent() override;
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnRoundStartedDynamicSignature OnRoundStarted;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnRoundFinishedDynamicSignature OnRoundFinished;
+
+	UFUNCTION(BlueprintCallable)
+	void StartRound();
+
+	UFUNCTION(BlueprintGetter)
+	int32 GetRoundIndex() const { return RoundIndex; }
+
+	UFUNCTION(BlueprintGetter)
+	FTimerHandle GetRoundTimer() const { return RoundTimer; }
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float RoundDuration = 10.0f;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintGetter=GetRoundIndex)
+	int32 RoundIndex = 0;
+
+	UPROPERTY(BlueprintGetter=GetRoundTimer)
+	FTimerHandle RoundTimer;
+
+	UFUNCTION()
+	void FinishRound();
+};
