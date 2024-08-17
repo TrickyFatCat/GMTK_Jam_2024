29c761b26387bedb2d32c36150fc2d3dfc3996ce
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-08-17 07:49:27 +0300

    Create ScaleBowl class

---
+++ Source/GMTK_Jam_2024/Actors/ScaleBowl.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScaleBowl.h"
+
+#include "EntityBase.h"
+#include "GMTK_Jam_2024/Components/EntityManagerComponent.h"
+#include "GMTK_Jam_2024/Components/WeightComponent.h"
+
+
+AScaleBowl::AScaleBowl()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>("Root");
+	SetRootComponent(Root);
+
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("StaticMeshComponent");
+	StaticMeshComponent->SetupAttachment(GetRootComponent());
+
+	WeightComponent = CreateDefaultSubobject<UWeightComponent>("WeightComponent");
+
+	EntityManagerComponent = CreateDefaultSubobject<UEntityManagerComponent>("EntityManager");
+}
+
+void AScaleBowl::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsValid(EntityManagerComponent))
+	{
+		EntityManagerComponent->OnEntityAdded.AddUniqueDynamic(this, &AScaleBowl::HandleEntityAdded);
+		EntityManagerComponent->OnEntityRemoved.AddUniqueDynamic(this, &AScaleBowl::HandleEntityRemoved);
+	}
+}
+
+void AScaleBowl::HandleEntityAdded(UEntityManagerComponent* EntityManagerComponent, AEntityBase* Entity)
+{
+	if(!IsValid(Entity))
+	{
+		return;
+	}
+
+	WeightComponent->AddWeight(Entity->GetCurrentWeight());
+}
+
+void AScaleBowl::HandleEntityRemoved(UEntityManagerComponent* EntityManagerComponent, AEntityBase* Entity)
+{
+	if (!IsValid(Entity))
+	{
+		return;
+	}
+
+	WeightComponent->RemoveWeight(Entity->GetCurrentWeight());
+}
+
---
+++ Source/GMTK_Jam_2024/Actors/ScaleBowl.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ScaleBowl.generated.h"
+
+class UEntityManagerComponent;
+class AEntityBase;
+
+UCLASS()
+class GMTK_JAM_2024_API AScaleBowl : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AScaleBowl();
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<USceneComponent> Root = nullptr;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UWeightComponent> WeightComponent = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UEntityManagerComponent> EntityManagerComponent = nullptr;
+
+private:
+	UFUNCTION()
+	void HandleEntityAdded(UEntityManagerComponent* EntityManagerComponent, AEntityBase* Entity);
+
+	UFUNCTION()
+	void HandleEntityRemoved(UEntityManagerComponent* EntityManagerComponent, AEntityBase* Entity);
+};
