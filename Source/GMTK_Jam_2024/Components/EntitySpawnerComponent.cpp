0345be45ac38787aec6d87c15bdba7e20d88a216
Artyom Volkov <tricky.fat.cat@gmail.com>
2024-08-17 12:06:38 +0300

    Create EntitySpawnComponent

---
+++ Source/GMTK_Jam_2024/Components/EntitySpawnerComponent.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EntitySpawnerComponent.h"
+
+#include "GMTK_Jam_2024/Actors/Entity.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+UEntitySpawnerComponent::UEntitySpawnerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+AEntity* UEntitySpawnerComponent::SpawnEntity(const FTransform& SpawnTransform)
+{
+	if (EntityClasses.IsEmpty())
+	{
+		return nullptr;
+	}
+
+	const TSubclassOf<AEntity> EntityClass = EntityClasses.Array()[UKismetMathLibrary::RandomInteger(EntityClasses.Num() - 1)];
+
+	if (!IsValid(EntityClass))
+	{
+		return nullptr;
+	}
+
+	AEntity* NewEntity = GetWorld()->SpawnActorDeferred<AEntity>(EntityClass, SpawnTransform);
+	NewEntity->SetOwner(GetOwner());
+	NewEntity->FinishSpawning(SpawnTransform);
+	OnEntitySpawned.Broadcast(this, NewEntity);
+	return NewEntity;
+}
---
+++ Source/GMTK_Jam_2024/Components/EntitySpawnerComponent.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "EntitySpawnerComponent.generated.h"
+
+class AEntity;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnEntitySpawnedDynamicSignatre,
+                                             UEntitySpawnerComponent*, Component,
+                                             AEntity*, Entity);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GMTK_JAM_2024_API UEntitySpawnerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UEntitySpawnerComponent();
+
+	UPROPERTY(BlueprintAssignable)
+	FOnEntitySpawnedDynamicSignatre OnEntitySpawned;
+	
+	UFUNCTION(BlueprintCallable, meta=(AutoCreateRefTerm="SpawnTransform"))
+	AEntity* SpawnEntity(const FTransform& SpawnTransform);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSet<TSubclassOf<AEntity>> EntityClasses;
+};
